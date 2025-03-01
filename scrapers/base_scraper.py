00839c79dfb895368b94133404a237ac5acd827f
Joaquin-Developer <joaquin.p.olivera@gmail.com>
2025-02-28 23:22:45 -0300

    APPS-21 - Permitir varios links para un mismo dominio - [Geant] Quitar EAN como parametro

--- scrapers/base_scraper.py
+++ scrapers/base_scraper.py
@@ -22,24 +22,53 @@ logging.basicConfig(level=logging.INFO)
 
 
 class BaseScrapper(ABC):
-    def __init__(self, url: str, wait_time: int = 3, save_in_json: bool = False, send_to_backend: bool = False):
-        self.url = url
+    def __init__(
+        self,
+        domain: str,
+        urls: List[str],
+        wait_time: int = 3,
+        save_in_json: bool = False,
+        send_to_backend: bool = False
+    ):
+        self.domain = domain
+        self.urls = urls
         self.wait_time = wait_time
         self.save_in_json = save_in_json
         self.send_to_backend = send_to_backend
+
         self.data: List[Dict[str, Any]] = []
+        self._json_data: List[Dict[str, Any]] = []
 
     @abstractmethod
-    def parse(self, page: Page):
+    def parse(self, page: Page) -> List[Dict[str, Any]]:
         raise NotImplementedError()
 
-    def run(self):
+    def scroll_to_bottom(self, page: Page, scrolls=5, delay=3):
+        for _ in range(scrolls):
+            page.evaluate("window.scrollTo(0, document.body.scrollHeight)")
+            time.sleep(delay)
+
+    def save_to_json(self):
+        """Save extracted data in JSON file"""
+        if not self.save_in_json:
+            return
+
+        output_file = f"data/raw/{self.__class__.__name__}_{datetime.now().strftime('%Y%m%d_%H:%M')}.json"
+        logging.info("Total elements scrapped: %i", len(self.data))
+
+        with open(output_file, "w", encoding="utf-8") as file:
+            json.dump(self._json_data, file, ensure_ascii=False, indent=4)
+
+    def send_data_to_backend(self):
+        return api.send_data_to_backend(self.data)
+
+    def _run(self, url: str) -> List[Dict[str, Any]]:
         with sync_playwright() as p:
             browser = p.chromium.launch(headless=True)
             page = browser.new_page()
 
-            logging.info(f"Scrapping: {self.url}")
-            page.goto(self.url)
+            logging.info(f"Scrapping: {url}")
+            page.goto(url)
 
             logging.info("Scrolling to the bottom of the page...")
             self.scroll_to_bottom(page)
@@ -48,40 +77,36 @@ class BaseScrapper(ABC):
             time.sleep(self.wait_time)
 
             logging.info("Extracting data...")
-            self.parse(page)
-
-            if self.data:
-                if self.save_in_json:
-                    logging.info("Saving data in JSON...")
-                    self.save_to_json()
-                else:
-                    print(self.data)
-
-                if self.send_data_to_backend:
-                    logging.info("Send data to Backend...")
-                    self.send_data_to_backend()
+            data = self.parse(page)
+            # self.data += data
 
             browser.close()
-            logging.info("End.")
+            return data
 
-    def scroll_to_bottom(self, page: Page, scrolls=5, delay=3):
-        for _ in range(scrolls):
-            page.evaluate("window.scrollTo(0, document.body.scrollHeight)")
-            time.sleep(delay)
+    def run(self):
+        for _url in self.urls:
+            url = self.domain + _url
 
-    def save_to_json(self):
-        """Guarda los datos extraídos en un archivo JSON."""
-        output_file = f"data/raw/{self.__class__.__name__}_{datetime.now().strftime('%Y%m%d_%H:%M')}.json"
+            try:
+                data = self._run(url)
+                if not data:
+                    continue
+                self.data += data
 
-        json_data = {
-            "url": self.url,
-            "data": self.data
-        }
+                if self.save_in_json:
+                    logging.info("Saving data in JSON...")
+                    self._json_data.append({
+                        "url": url,
+                        "data": data
+                    })
 
-        logging.info("Total elements scrapped: %i", len(self.data))
+            except Exception as error:
+                logging.warning("Failed scrapping for %s", url)
+                logging.warning(str(error))
 
-        with open(output_file, "w", encoding="utf-8") as file:
-            json.dump(json_data, file, ensure_ascii=False, indent=4)
+        self.save_to_json()
 
-    def send_data_to_backend(self):
-        return api.send_data_to_backend(self.data)
+        if self.data and self.send_data_to_backend:
+            logging.info("Send data to Backend...")
+            self.send_data_to_backend()
+        logging.info("End.")
--- scrapers/geant.py
+++ scrapers/geant.py
@@ -1,16 +1,16 @@
+from typing import List, Dict, Any
+
 from base_scraper import BaseScrapper, Page
 
 
 class Geant(BaseScrapper):
     SUPERMARKET_CHAIN_ID = 1
 
-    def parse(self, page: Page):
+    def parse(self, page: Page) -> List[Dict[str, Any]]:
+        data = []
         articles = page.query_selector_all(
             ".vtex-product-summary-2-x-element.pointer.pt3.pb4.flex.flex-column.h-100"
         )
-
-        # TODO permitir ean null en el backend
-        ean = 1111
 
         for article in articles:
             name = article.query_selector(".vtex-product-summary-2-x-productBrand").inner_text()
@@ -20,18 +20,21 @@ class Geant(BaseScrapper):
             price = article.query_selector(".devotouy-products-components-0-x-sellingPriceWithUnitMultiplier")
             price = price.inner_text().replace("\n", " ") if price else None
 
-            self.data.append({
-                "ean": str(ean),
+            data.append({
                 "name": name,
                 "imageLink": image_link,
                 "currency": price.split(" ")[0],
-                "price": price.split(" ")[1],
+                "price": price.split(" ")[1].replace(".", ""),
                 "supermarketChainId": self.SUPERMARKET_CHAIN_ID,
             })
-            ean += 1
+        return data
 
 
 if __name__ == "__main__":
-    url = "https://www.geant.com.uy/frescos/congelados/pre-fritos-rebozados"
-    scraper = Geant(url=url, save_in_json=True)
+    domain = "https://www.geant.com.uy"
+    urls = [
+        "/frescos/congelados/pre-fritos-rebozados",
+        "/frescos/congelados/pescados-congelados",
+    ]
+    scraper = Geant(domain, urls, save_in_json=True)
     scraper.run()
